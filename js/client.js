3d091ed63759ed6c5c9176dfc0a62199281d7ce5
Ankit2885 <ak176140@gmail.com>
2022-05-01 17:54:26 +0530

    first commit

---
+++ js/client.js
@@ -0,0 +1,40 @@
+const socket = io('http://localhost:8000', { transports : ['websocket'] });
+
+const from = document.getElementById('send-container');
+const messageInput = document.getElementById('messageInp');
+const messageContainer = document.querySelector(".container");
+var audio = new Audio('chatring.mp3');
+
+const append = (message, position)=>{
+    const messageElement = document.createElement('div');
+    messageElement.innerHTML = message;
+    messageElement.classList.add('message'); 
+    messageElement.classList.add(position);
+    messageContainer.append(messageElement); 
+    if(position == 'left'){
+        audio.play();
+    }
+}
+
+from.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    const message = messageInput.value;
+    append(`You: ${message}`, 'right');
+    socket.emit('send', message);
+    messageInput.value = "";
+})
+
+const name = prompt("Enter your name to join");
+socket.emit('new-user-joined', name);
+
+socket.on('user-joined', name=>{
+    append(`${name} joined the chat`, "right")
+})
+
+socket.on('receive', data=>{
+    append(`${data.name}: ${data.message}`, "left")
+})
+
+socket.on('leave', data=>{
+    append(`${name} left the chat`, "left")
+})
