4f02b004c0c41a0e28db222c6cce2190ae913cff
vega6Ankit <ankit@vega6.com>
2024-06-14 11:12:44 +0530

    update change4ws

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
 // Node server which will handel socket io connections
 
 const io = require('socket.io')(8000);
-const express = require('express')
-const app = express()
-const path = require('path')
+const express = require('express');
+const app = express();
+const path = require('path');
 const port = process.env.PORT || 3000;
 
-app.use('/', express.static(path.join(__dirname,"public")));
-console.log(path.join(__dirname,"public"));
+app.use('/', express.static(path.join(__dirname, "public")));
+console.log(path.join(__dirname, "public"));
 
 const users = {};
 
-io.on('connection', socket =>{
-    socket.on('new-user-joined', name =>{
+io.on('connection', socket => {
+    socket.on('new-user-joined', name => {
         // console.log("new user", name);
         users[socket.id] = name;
         socket.broadcast.emit('user-joined', name)
     })
-    socket.on('send', message =>{
-        socket.broadcast.emit('receive', {message: message, name: users[socket.id]})
+    socket.on('send', message => {
+        socket.broadcast.emit('receive', { message: message, name: users[socket.id] })
     })
-    socket.on('disconnect', message =>{
+    socket.on('disconnect', message => {
         socket.broadcast.emit('leave', users[socket.id]);
         delete users[socket.id];
     })
@@ -28,4 +28,4 @@ io.on('connection', socket =>{
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-  })
+})
